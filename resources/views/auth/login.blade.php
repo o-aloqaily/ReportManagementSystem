8648d775392a7c95a2ff4facde2e760a0c19257c
o-aloqaily <osamaoqaily@gmail.com>
2019-03-26 12:02:29 +0300

    hide password reset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,11 +59,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
